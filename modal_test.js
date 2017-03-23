bc1c5ecf95bb6f9e17c776cfcc1ce65fb2c29105
mjyaiun <1019807893@qq.com>
2017-03-23 14:36:26 +0800

    Create modal_test.js

---
+++ modal_test.js
@@ -0,0 +1,12 @@
+/* 打开模态框 */
+/* 1.验证模态框中的form表单 */
+/* 2.ajax提交 */
+function openModal(){
+  alert("modal已打开，处理事情");
+}
+
+/* 关闭模态框 */
+/* 1.清空模态框中form表单数据 */
+function openModal(){
+  alert("modal已关闭，处理事情");
+}
